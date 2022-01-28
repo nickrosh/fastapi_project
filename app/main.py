394970bef6fb038890e26f8e647b68a5737f60cb
Nick Roshdieh <nickroshd@gmail.com>
2022-01-28 01:53:39 -0500

    split schemas to a module

--- app/main.py
+++ app/main.py
@@ -6,77 +6,67 @@ import time
 from typing import Optional
 from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
-from pydantic import BaseModel
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from dotenv import load_dotenv
-from app import models
+from app import models, schemas
 from app.database import engine, get_db
 from sqlalchemy.orm import Session
 
-load_dotenv()
+# load_dotenv()
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
 
-retries = 5
-while retries > 0:
-    try:
-        connection = psycopg2.connect(
-            host="host.docker.internal",
-            database="fastapi",
-            user=os.getenv("POSTGRES_USER"),
-            password=os.getenv("POSTGRES_PASSWORD"),
-            cursor_factory=RealDictCursor
-        )
-        cursor = connection.cursor()
-        print("database connection established")
-        # cursor.close()
-        # connection.close()
-        break
+# retries = 5
+# while retries > 0:
+#     try:
+#         connection = psycopg2.connect(
+#             host="host.docker.internal",
+#             database="fastapi",
+#             user=os.getenv("POSTGRES_USER"),
+#             password=os.getenv("POSTGRES_PASSWORD"),
+#             cursor_factory=RealDictCursor
+#         )
+#         cursor = connection.cursor()
+#         print("database connection established")
+#         # cursor.close()
+#         # connection.close()
+#         break
 
-    except ConnectionError as error:
-        print("database connection failed")
-        print(f"Error: {error}")
-        retries -= 1
-        time.sleep(2)
-
-
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True
+#     except ConnectionError as error:
+#         print("database connection failed")
+#         print(f"Error: {error}")
+#         retries -= 1
+#         time.sleep(2)
 
 
-my_posts = [
-    {"title": "title of post 1", "content": "content of post 1", "id": 1},
-    {"title": "title of post 2", "content": "content of post 2", "id": 2}
-    ]
 
 
-def find_post(id):
-    for p in my_posts:
-        if p['id'] == id:
-            return p
+
+# my_posts = [
+#     {"title": "title of post 1", "content": "content of post 1", "id": 1},
+#     {"title": "title of post 2", "content": "content of post 2", "id": 2}
+#     ]
 
 
-def find_post_index(id):
-    for i, p in enumerate(my_posts):
-        if p['id'] == id:
-            return i
+# def find_post(id):
+#     for p in my_posts:
+#         if p['id'] == id:
+#             return p
+
+
+# def find_post_index(id):
+#     for i, p in enumerate(my_posts):
+#         if p['id'] == id:
+#             return i
 
 
 @app.get("/")
 def root():
-    return {"message": "SANDOSIUS"}
-
-
-@app.get("/sqlalchemy")
-def test_posts(db: Session = Depends(get_db)):
-    posts = db.query(models.Post).all()
-    return {"data": posts}
+    return {"message": "Welcome to the API"}
 
 
 @app.get("/posts")
@@ -88,6 +78,7 @@ def get_posts(db: Session = Depends(get_db)):
 @app.get("/posts/{id}")
 def get_post(id: int, db: Session = Depends(get_db)):
     post_detail = db.query(models.Post).filter(models.Post.id == id).first()
+
     if not post_detail:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} was not found")
@@ -96,7 +87,7 @@ def get_post(id: int, db: Session = Depends(get_db)):
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_post(post: Post, db: Session = Depends(get_db)):
+def create_post(post: schemas.Post, db: Session = Depends(get_db)):
     new_post = models.Post(**post.dict())
     db.add(new_post)
     db.commit()
@@ -106,7 +97,7 @@ def create_post(post: Post, db: Session = Depends(get_db)):
 
 
 @app.put("/posts/{id}")
-def update_post(id: int, post: Post, db: Session = Depends(get_db)):
+def update_post(id: int, post: schemas.Post, db: Session = Depends(get_db)):
     updated_post = db.query(models.Post).filter(models.Post.id == id)
 
     if updated_post.first() == None:
