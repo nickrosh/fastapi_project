f79845a1ca03f15206fcc41a17019c8c1fa541b8
Nick Roshdieh <nickroshd@gmail.com>
2022-01-30 19:30:34 -0500

    commented out CORS for future use

--- app/main.py
+++ app/main.py
@@ -15,13 +15,13 @@ origins = [
 ]
 
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=['*'],
-    allow_headers=['*']
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=['*'],
+#     allow_headers=['*']
+# )
 
 
 app.include_router(post.router)
