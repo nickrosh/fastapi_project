7d1cea283a94bf57ebfaac7e0af9e5ad83276536
Nick Roshdieh <nickroshd@gmail.com>
2022-02-06 01:17:57 -0500

    moved db code and fixtures to database.py

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,45 +1,5 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from fastapi.testclient import TestClient
-import pytest
-from app.main import app
-from app import schemas, models
-from app.config import settings
-from app.database import get_db
-
-
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.DATABASE_USER}:{settings.DATABASE_PASSWORD}@{settings.DATABASE_HOSTNAME}:{settings.DATABASE_PORT}/{settings.DATABASE_NAME}_test'
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
-
-
-@pytest.fixture
-def session():
-    # before our tests drop all tables so we have a clean slate, then create
-    # all tables. This way we can also see the tables after the tests are done.
-    models.Base.metadata.drop_all(bind=engine)
-    models.Base.metadata.create_all(bind=engine)
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-@pytest.fixture
-def client(session):
-    def override_get_db():
-        try:
-            yield session
-        finally:
-            session.close()
-    app.dependency_overrides[get_db] = override_get_db
-    yield TestClient(app)
+from app import schemas
+from tests.database import client, session
 
 
 def test_root(client):
